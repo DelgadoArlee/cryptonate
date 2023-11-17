e496865e405f71aa0a9c8aaa0627f7972b0d11d6
Jhurds <georgejoseabaygar@gmail.com>
2023-11-17 11:55:58 +0800

    Intializes firebase

---
+++ src/api/firebase.main.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "dotenv/config";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
