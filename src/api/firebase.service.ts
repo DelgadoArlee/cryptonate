8ce8e27057313ec41305fb80352ae135deb64096
Jhurds <georgejoseabaygar@gmail.com>
2023-11-18 01:46:54 +0800

    Adds getTransactions as leaderboard

--- src/api/firebase.service.ts
+++ src/api/firebase.service.ts
@@ -1,18 +1,63 @@
-import { Database, DatabaseReference, ref, set } from 'firebase/database';
-import { Transaction } from './models/transaction.entity';
+import {
+  DataSnapshot,
+  Database,
+  DatabaseReference,
+  onValue,
+  push,
+  ref,
+} from 'firebase/database';
+import { Donation } from './models/donation.entity';
 
 export class FirebaseService {
   constructor(private readonly db: Database) {}
 
-  async addTransaction(donor: string, amount: number) {
-    const donation = new Transaction(donor, amount);
+  async addTransaction(userId: string, amount: number): Promise<void> {
     const reference: DatabaseReference = ref(
       this.db,
-      'donations/' + donation.donor,
+      'transactions/' + userId + '/donations',
     );
 
-    set(reference, donation);
+    const donation = new Donation(amount);
+
+    await push(reference, donation);
   }
 
-  async getTransactions() {}
+  async getTransactions(): Promise<{ [userId: string]: number }> {
+    const reference: DatabaseReference = ref(this.db, 'transactions/');
+
+    // Create a promise to handle the asynchronous operation
+    const leaderboardPromise = new Promise<{ [userId: string]: number }>(
+      (resolve, reject) => {
+        onValue(
+          reference,
+          (usersSnapshot: DataSnapshot) => {
+            const leaderboard: { [userId: string]: number } = {};
+
+            usersSnapshot.forEach((userSnapshot: DataSnapshot) => {
+              const userId = userSnapshot.key as string;
+              const userDonations = Object.values(
+                userSnapshot.val().donations,
+              ) as Donation[];
+
+              // Calculate the total donation amount for each user
+              let totalAmount = 0;
+              userDonations.forEach((donation: Donation) => {
+                totalAmount += donation.amount;
+              });
+
+              leaderboard[userId] = totalAmount;
+            });
+
+            resolve(leaderboard);
+          },
+          (error) => {
+            reject(error);
+          },
+        );
+      },
+    );
+
+    // Return the leaderboard
+    return leaderboardPromise;
+  }
 }
