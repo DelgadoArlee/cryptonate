b52d6ad3ac3e7aa7ff0b481c67edd6bca6813c6c
Jhurds <georgejoseabaygar@gmail.com>
2023-11-18 00:25:29 +0800

    Adds Id to transaction model

--- src/api/models/transaction.entity.ts
+++ src/api/models/transaction.entity.ts
@@ -1,9 +1,12 @@
 export class Transaction {
+  readonly id: string;
+
   readonly donor: string;
 
   readonly amount: number;
 
-  constructor(donor: string, amount: number) {
+  constructor(id: string, donor: string, amount: number) {
+    this.id = id;
     this.donor = donor;
     this.amount = amount;
   }
