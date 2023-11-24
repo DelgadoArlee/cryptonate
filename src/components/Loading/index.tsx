095c2cb5f39da5cb1bc5a404ddc5c8df989f6ed0
DelgadoArlee <delgadoarlee@gmail.com>
2023-11-24 22:28:36 +0800

    Removed unused code

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,25 +1,9 @@
-import { Grid, CircularProgress, Box } from '@mui/material';
+import { CircularProgress, Box } from '@mui/material';
 
 function Loading() {
   return (
     <Box>
       <CircularProgress size={150} />
-    </Box>
-  );
-  return (
-    <Box sx={{ padding: '25%' }}>
-      <Grid
-        container
-        sx={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          alignContent: 'center',
-        }}
-      >
-        <Grid item>
-          <CircularProgress size={150} />
-        </Grid>
-      </Grid>
     </Box>
   );
 }
