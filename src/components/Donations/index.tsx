177f7134b88e7e39a9a530b541f0e39dcef33ca0
DelgadoArlee <delgadoarlee@gmail.com>
2023-11-25 00:58:38 +0800

    Set donations to refetch on change

--- src/components/Donations/index.tsx
+++ src/components/Donations/index.tsx
@@ -1,25 +1,37 @@
-import { useBalance } from 'wagmi'
-import Loading from '../Loading'
-import { Typography, Grid } from '@mui/material'
+import { useBalance } from 'wagmi';
+import Loading from '../Loading';
+import { Typography, Grid } from '@mui/material';
 
 function Donations() {
-    const { data, isError, isLoading } = useBalance({
-      address: '0x00e2560fFE320cE84Cc2F1C71E6563CBb6D465b2',
-    })
-   
-    if (isLoading) return <Loading/>
-    if (isError) return <div>Error fetching balance</div>
+  const { data, isError, isLoading } = useBalance({
+    address: '0x00e2560fFE320cE84Cc2F1C71E6563CBb6D465b2',
+    watch: true,
+  });
 
-    const donations = data? `${parseFloat(data?.formatted).toFixed(2)} ${data?.symbol}`: 'Connection Error'
+  if (isLoading) return <Loading />;
+  if (isError) return <div>Error fetching balance</div>;
 
-    return (
-      <>
+  const donations = data
+    ? `${parseFloat(data?.formatted).toFixed(2)} ${data?.symbol}`
+    : 'Connection Error';
+
+  return (
+    <>
       <Grid>
-        <Typography variant="h4" align="center" fontWeight='bold'>DONATIONS</Typography>
-        <Typography sx={{borderBottom: 2, paddingBottom: 2}} variant="h5" align="center" fontWeight='bold'>{donations}</Typography>
+        <Typography variant="h4" align="center" fontWeight="bold">
+          DONATIONS
+        </Typography>
+        <Typography
+          sx={{ borderBottom: 2, paddingBottom: 2 }}
+          variant="h5"
+          align="center"
+          fontWeight="bold"
+        >
+          {donations}
+        </Typography>
       </Grid>
-      </>
-    )
+    </>
+  );
 }
 
-export default Donations
+export default Donations;
